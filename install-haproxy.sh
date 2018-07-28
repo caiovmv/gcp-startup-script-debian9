87f36940363258eef04d56423fa1c14dd376713e
Caio Villela <villela.caio@gmail.com>
2018-07-28 08:28:57 -0300

    Update install-haproxy.sh

--- install-haproxy.sh
+++ install-haproxy.sh
@@ -3,4 +3,4 @@ echo deb http://httpredir.debian.org/debian stretch-backports main | tee /etc/ap
 curl https://haproxy.debian.net/bernat.debian.org.gpg | apt-key add -
 echo deb http://haproxy.debian.net stretch-backports-1.8 main | tee /etc/apt/sources.list.d/haproxy.list
 apt-get update
-apt-get install haproxy=1.8.\* -t stretch-backports
+apt-get install -y haproxy=1.8.\* -t stretch-backports
